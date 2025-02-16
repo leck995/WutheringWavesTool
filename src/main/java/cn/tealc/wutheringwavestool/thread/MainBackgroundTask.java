e13a643a12b3937883d5afcdf722899492e98a67
Leck995 <yiqiu995@gmail.com>
2025-02-16 22:03:47 +0800

    调整背景模糊的参数

--- src/main/java/cn/tealc/wutheringwavestool/thread/MainBackgroundTask.java
+++ src/main/java/cn/tealc/wutheringwavestool/thread/MainBackgroundTask.java
@@ -34,7 +34,7 @@ public class MainBackgroundTask extends Task<Background> {
         this.image = image;
         gaussianFilter = new GaussianFilter(16);
         contrastFilter = new ContrastFilter();
-        contrastFilter.setBrightness(0.8f);
+        contrastFilter.setBrightness(1.0f);
         contrastFilter.setContrast(1.3f);
     }
 
