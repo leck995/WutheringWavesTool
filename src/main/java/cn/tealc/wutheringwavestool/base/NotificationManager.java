5a40595a79be11fa73d3d8015e56f85c71823e75
Leck995 <yiqiu995@gmail.com>
2025-02-18 01:10:11 +0800

    优化全局通知工具类，添加订阅与发布

--- src/main/java/cn/tealc/wutheringwavestool/base/NotificationManager.java
+++ src/main/java/cn/tealc/wutheringwavestool/base/NotificationManager.java
@@ -1,7 +1,9 @@
 package cn.tealc.wutheringwavestool.base;
 
 import cn.tealc.wutheringwavestool.model.message.MessageInfo;
+import com.jfoenixN.controls.JFXDialogLayout;
 import de.saxsys.mvvmfx.MvvmFX;
+import de.saxsys.mvvmfx.utils.notifications.NotificationObserver;
 
 /**
  * @program: WutheringWavesTool
@@ -10,7 +12,19 @@ import de.saxsys.mvvmfx.MvvmFX;
  * @create: 2024-12-17 17:10
  */
 public class NotificationManager {
+    public static void publish(String key, Object... objects) {
+        MvvmFX.getNotificationCenter().publish(key, objects);
+    }
+
+    public static void subscribe(String key, NotificationObserver observer) {
+        MvvmFX.getNotificationCenter().subscribe(key, observer);
+    }
+
     public static void message(MessageInfo messageInfo) {
         MvvmFX.getNotificationCenter().publish(NotificationKey.MESSAGE, messageInfo);
+    }
+
+    public static void dialog(JFXDialogLayout layout) {
+        MvvmFX.getNotificationCenter().publish(NotificationKey.DIALOG, layout);
     }
 }
