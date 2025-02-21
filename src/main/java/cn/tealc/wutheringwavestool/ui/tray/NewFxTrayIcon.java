6f489d74905e8d04794bcf793ec787cd7c42692e
Leck995 <yiqiu995@gmail.com>
2025-02-21 18:16:18 +0800

    修复因为托盘不适配高DPI导致系统级缩放后无法右键出现托盘菜单的问题

--- src/main/java/cn/tealc/wutheringwavestool/ui/tray/NewFxTrayIcon.java
+++ src/main/java/cn/tealc/wutheringwavestool/ui/tray/NewFxTrayIcon.java
@@ -2,10 +2,12 @@ package cn.tealc.wutheringwavestool.ui.tray;
 
 import cn.tealc.wutheringwavestool.FXResourcesLoader;
 import javafx.application.Platform;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -23,11 +25,10 @@ public class NewFxTrayIcon extends TrayIcon {
     private final StackPane pane = new StackPane();
     private Stage parent;
 
-    public NewFxTrayIcon(Image image, String tooltip,Region menu) {
+    public NewFxTrayIcon(Image image, String tooltip, Region menu) {
         super(image, tooltip);
         //设置系统托盘图标为自适应
         this.setImageAutoSize(true);
-
         initStage();
         //添加组件到面板中
         pane.getChildren().add(menu);
@@ -42,8 +43,9 @@ public class NewFxTrayIcon extends TrayIcon {
                 //getButton() 1左键 2中键 3右键
                 if (e.getButton() == 3) {
                     Platform.runLater(() -> {
-                        stage.setX(e.getX());
-                        stage.setY(e.getY() - stage.getHeight());
+                        Point2D scale = getScale();
+                        stage.setX(e.getX() / scale.getY() - 5);
+                        stage.setY((e.getY() - stage.getHeight()) / scale.getY() - 5);
                         if (!stage.isShowing()) {
                             stage.show();
                         } else {
@@ -56,10 +58,22 @@ public class NewFxTrayIcon extends TrayIcon {
     }
 
 
-    private void initStage(){
+    /**
+     * @description: 获取系统缩放比例
+     * @param:
+     * @return  javafx.geometry.Point2D
+     * @date:   2025/2/21
+     */
+    private Point2D getScale() {
+        double scaleX = Screen.getPrimary().getOutputScaleX();
+        double scaleY = Screen.getPrimary().getOutputScaleY();
+        return new Point2D(scaleX, scaleY);
+    }
+
+    private void initStage() {
         parent = new Stage();
         parent.setTitle("鸣潮助手 Tray Parent");
-        stage.getIcons().add(new javafx.scene.image.Image(FXResourcesLoader.load("image/icon.png"),45,45,true,true));
+        stage.getIcons().add(new javafx.scene.image.Image(FXResourcesLoader.load("image/icon.png"), 45, 45, true, true));
         parent.setWidth(1.0);
         parent.setHeight(1.0);
         parent.initStyle(StageStyle.UTILITY);
@@ -71,7 +85,7 @@ public class NewFxTrayIcon extends TrayIcon {
         stage.setTitle("鸣潮助手 Tray");
 
         stage.setScene(scene);
-        stage.getIcons().add(new javafx.scene.image.Image(FXResourcesLoader.load("image/icon.png"),45,45,true,true));
+        stage.getIcons().add(new javafx.scene.image.Image(FXResourcesLoader.load("image/icon.png"), 45, 45, true, true));
         stage.initStyle(StageStyle.UNDECORATED);
         stage.initOwner(parent);
         stage.setAlwaysOnTop(true); //设置为顶层，否则在windows系统中会被底部任务栏遮挡
