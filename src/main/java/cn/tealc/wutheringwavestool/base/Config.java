6ab33cfe127d76b842204c3233731f2a0b60c726
leck995 <yiqiu995@gmail.com>
2025-09-07 16:18:18 +0800

    版本号至1.3.2

--- src/main/java/cn/tealc/wutheringwavestool/base/Config.java
+++ src/main/java/cn/tealc/wutheringwavestool/base/Config.java
@@ -16,7 +16,7 @@ import java.util.ResourceBundle;
  * @create: 2024-07-03 00:37
  */
 public class Config {
-    public static final String version = "1.3.1";
+    public static final String version = "1.3.2";
     public static final String appAuthor = "Leck";
 
     public static final String apiDecryptKey = "XSNLFgNCth8j8oJI3cNIdw==";
