d54368413a4117c3ae401966bc99d9f3cae36bed
leck <yiqiu995@gmail.com>
2026-06-08 15:04:26 +0800

    版本号到1.4.1

--- src/main/java/cn/tealc/wutheringwavestool/base/AppConstants.java
+++ src/main/java/cn/tealc/wutheringwavestool/base/AppConstants.java
@@ -3,7 +3,7 @@ package cn.tealc.wutheringwavestool.base;
 public final class AppConstants {
     private AppConstants() {}
 
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.4.1";
     public static final String APP_AUTHOR = "Leck";
 
     public static final String API_DECRYPT_KEY = "XSNLFgNCth8j8oJI3cNIdw==";
